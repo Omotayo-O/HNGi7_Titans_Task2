6b3a12a54d7514f3240111596e4896afa8e520cb
Omotayo-O <61963471+Omotayo-O@users.noreply.github.com>
2020-06-07 16:11:41 +0100

    Update omotayo.js

--- scripts/omotayo.js
+++ scripts/omotayo.js
@@ -1,1 +1,14 @@
+var person = {
+    name:  'Omotayo Adefolalu',
+    HNG_ID: 'HNG-04348',
+    email: 'omotayo.na@gmail.com',
+    slack_id: 'omotayo',
+    language: 'javascript'
+  };
 
+  function personinfo(person){
+   return "Hello World, this is "+person.name+ " with HNGi7 ID " + person.HNG_ID + " and email " + person.email+ " using "+ person.language+" for stage 2 task";
+  }
+
+
+  console.log(personinfo(person));
